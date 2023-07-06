fdb56889e24fb72cf61aa75e53fa6911c99aec10
ibrohim795 <iismatov59@gmail.com>
2023-07-06 18:37:34 +0500

    max min

--- lession02/Program.cs
+++ lession02/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace lession02
+﻿namespace Lession02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ismatov");
+
+            Byte ByteMax = Byte.MaxValue;
+            Byte ByteMin = Byte.MinValue;
+            Console.WriteLine("Byte max: " + ByteMax);
+            Console.WriteLine("byte min: " + ByteMin);
+            short shortMax = short.MaxValue;
+            short shortMin = short.MinValue;
+            Console.WriteLine("short max: " + shortMax);
+            Console.WriteLine("short min: " + shortMin);
+            int intMax = int.MaxValue;
+            int intMin=int.MinValue;
+            Console.WriteLine("Int max: "+intMax);
+            Console.WriteLine("Int min: "+intMin);
+            long longMax = long.MaxValue;
+            long longMin = long.MinValue;
+            Console.WriteLine("long max: "+longMax);
+            Console.WriteLine("long min: "+longMin);
+            float floatMax = float.MaxValue;
+            float floatMin = float.MinValue;
+            Console.WriteLine("float max: "+floatMax);
+            Console.WriteLine("float min: "+floatMin);
+            double doubleMax = double.MaxValue;
+            double doubleMin = double.MinValue;
+            Console.WriteLine("double max: "+doubleMax);
+            Console.WriteLine("double min: "+doubleMin);
+
+
         }
     }
 }
